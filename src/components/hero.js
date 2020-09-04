eea087cf335cc0c6bb022a8b90d7d205b11c4b35
Rupal Jain <rupaljain.1699@gmail.com>
2020-09-05 04:06:20 +0530

    hero and skills' section started

---
+++ src/components/hero.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import './hero.css'
+
+function hero() {
+    return (
+           <section className="hero">
+               <div className="hero__container hero__content">
+                   <div className="">
+                       <h1>Hello! <span></span></h1>
+                       <h1>My name is <span></span></h1>
+                       <h1>Rupal Jain <span></span></h1>
+                       <a href="#" type="button" className="hero__btn">Portfolio</a>
+                   </div>
+
+               </div>
+           </section>
+    )
+}
+
+export default hero
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,19 +4,24 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Hero from "../components/hero"
+import Skills from "../components/skills"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
+  <div>
+
+    {/* <SEO title="Home" /> */}
+    <Hero />
+    <Skills />
+    {/* <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.This is an extra line</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+    <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
+  </div>
 )
 
 export default IndexPage
